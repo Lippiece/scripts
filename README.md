84a2b1c1cd7d67a2af0b29248ed624bb436c845d
lippiece <github@lippiece.anonaddy.me>
2025-08-14 17:21:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# scripts
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+This project was created using `bun init` in bun v1.2.20. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
---
+++ src/filterHckrnews.ts
@@ -0,0 +1,39 @@
+const ignorelist = new Set([
+  "AI",
+  "Amazon",
+  "Apple",
+  "Biden",
+  "Elon",
+  "Facebook",
+  "Google",
+  "IBM",
+  "Intel",
+  "Kanye",
+  "Meta",
+  "Microsoft",
+  "Musk",
+  "Netflix",
+  "Nvidia",
+  "Oracle",
+  "Putin",
+  "SpaceX",
+  "Tesla",
+  "Trump",
+  "Twitter",
+  "Yahoo",
+  "Zuckerberg",
+])
+const stories    = document.querySelectorAll(".link.story")
+
+for (const link of Array.from(stories)) {
+  if (
+    link.closest("li")?.style.display !== "none" &&
+    link instanceof HTMLElement
+  ) {
+    const text = link.textContent
+
+    if (text && ignorelist.has(text)) {
+      ;(link.closest("li") || link).style.opacity = "0.5"
+    }
+  }
+}
