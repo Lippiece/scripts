487cfd6790a974298e07c37816b593fe67f62e97
lippiece <github@lippiece.anonaddy.me>
2025-10-11 12:41:44 +0200

    feat(scripts): add skip max on gosuslugi script

---
+++ src/gosuslugiSkipMax.ts
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        Skip Max messenger link on gosuslugi.ru login
+// @namespace   Violentmonkey Scripts
+// @match       https://esia.gosuslugi.ru/login/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 11/10/2025, 12:14:08
+// ==/UserScript==
+
+const root = document.querySelector("esia-root")
+
+// Ugly.
+const observer = new MutationObserver(mutations => {
+  for (const mutation of mutations) {
+    for (const node of mutation.addedNodes) {
+      if (
+        node instanceof HTMLElement &&
+        node.matches("div.mt-40:has([href='https://download.max.ru/'])")
+      ) {
+        const skipButton = node.querySelector("button")
+
+        skipButton instanceof HTMLButtonElement && skipButton.click()
+      }
+    }
+  }
+})
+
+root && observer.observe(root, { childList: true, subtree: true })
