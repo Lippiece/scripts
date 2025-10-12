1349961d48ef81f8348fec4183afbf99923fce52
lippiece <github@lippiece.anonaddy.me>
2025-10-13 01:06:08 +0200

    feat(scripts): add invidious dislikes script rewrite
    thanks dieser-niko

---
+++ src/invidiousDislikes.ts
@@ -0,0 +1,24 @@
+//! ==UserScript==
+//! @name         Return YouTube Dislike for Invidious
+//! @version      0.2
+//! @description  Show dislikes for a video on invidious instances (NOT nadeko.net)
+//! @author       dieser-niko
+//! @match        https://*/watch?*v=*
+//! @connect      returnyoutubedislikeapi.com
+//! @icon         https://invidious.io/favicon-32x32.png
+//! @top-level-await
+//! ==/UserScript==
+
+const apiUrl          = "https://returnyoutubedislikeapi.com/votes?videoId="
+const { videoId }     = JSON.parse(
+  document.querySelector("#video_data")?.innerHTML || "{}",
+)
+const { dislikes }    = await fetch(apiUrl + videoId).then(({ json }) => json())
+const likesElement    = document.querySelector("#likes")
+const dislikesElement = document.createElement("p")
+const dislikeIcon     = document.createElement("i")
+
+dislikesElement.textContent = dislikes
+dislikeIcon.classList.add("icon", "ion-ios-thumbs-down")
+dislikesElement.append(dislikeIcon)
+likesElement?.after(dislikesElement)
