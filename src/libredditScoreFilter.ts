8c8797db3204e77e60dd6c32e05757eee0661caf
lippiece <github@lippiece.anonaddy.me>
2026-01-03 19:08:06 +0200

    feat(libreddit-filter): also hide "hidden" score posts

--- src/libredditScoreFilter.ts
+++ src/libredditScoreFilter.ts
@@ -10,9 +10,10 @@ const scoreElements = document.body.querySelectorAll(".post_score")
 
 for (const element of scoreElements) {
   if (element instanceof HTMLElement) {
-    const score = Number(element.title)
+    const score         = Number(element.title)
+    const isHiddenScore = element.title === "Hidden"
 
-    if (score < 1000) {
+    if (score < 1000 || isHiddenScore) {
       const postElement = element.closest(".post")
 
       postElement?.remove()
