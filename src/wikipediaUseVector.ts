1e37879d99059bb8fd81b7d0cf8e0c48e0e11748
lippiece <github@lippiece.anonaddy.me>
2026-03-08 11:34:16 +0200

    feat(wikipediaUseVector): add script to set wikipedia's skin to `vector-2022`

---
+++ src/wikipediaUseVector.ts
@@ -0,0 +1,17 @@
+//! ==UserScript==
+//! @name        Set Wikipedia skin to `vector-2022`
+//! @match       https://*.wikipedia.org/*
+//! @icon        https://wikipedia.org/static/favicon/wikipedia.ico
+//! @grant       none
+//! @version     v1.8.1
+//! @author      lippiece
+//! ==/UserScript==
+
+const url       = new URL(location.href)
+const skin      = "vector-2022"
+const hasVector = url.searchParams.get("useskin") === skin
+
+if (!hasVector) {
+  url.searchParams.set("useskin", skin)
+  location.href = url.toString()
+}
